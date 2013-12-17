0d150bea709b07629536ffb16967746dde2adf24
Moshe Bergman <bergmanm@gmail.com>
2013-12-16 20:15:22 -0800

    executable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # global install: nodejs, grunt, grunt-cli, forever, coffee-script, bower
 # copy config
+git pull
 npm install --production
 grunt build
 NODE_ENV=production forever start -c coffee server.coffee
